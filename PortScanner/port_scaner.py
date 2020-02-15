352d6da23aa3f82725b9bec2a89ef499b74c9801
Parrot User <user@localhost.localdomain>
2020-02-14 23:55:00 -0500

    Threading added

--- PortScanner/port_scaner.py
+++ PortScanner/port_scaner.py
@@ -1,20 +1,28 @@
 from socket import *
 import argparse
 import sys
+from threading import Thread,Semaphore
 
+screenlock=Semaphore(value=1)
 def conn_scan(host,port):
     try:
         skt=socket(AF_INET,SOCK_STREAM)
         skt.connect((host,port))
         skt.send('ThirdNitrogen\n'.encode())
         result=skt.recv(1000)
+        screenlock.acquire()
+        print("Scanning port "+str(port))
         print("Port {0}/TCP open".format(port))
         print("[+] "+str(result))
         skt.close()
     except:
         e=sys.exc_info()
-        # print(e)
+        screenlock.acquire()
+        print(e)
         print("Port {0}/TCP closed".format(port))
+    finally:
+        screenlock.release()
+        skt.close()
 
 def port_scan(host,ports):
     try:
@@ -28,14 +36,15 @@ def port_scan(host,ports):
         print("[+] Scan result for {0}".format(target_ip))
     setdefaulttimeout(10)
     for port in ports:
-        print("Scanning port "+port)
-        conn_scan(target_ip,int(port))
+        # conn_scan(target_ip,int(port))
+        thread=Thread(target=conn_scan,args=(host,int(port)))
+        thread.start()
 
 
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument('-H','--host',help='Target host',default='127.0.0.1')
-    parser.add_argument('-p','--port',help='Target port(s)',default='80,21,22')
+    parser.add_argument('-p','--port',help='Target port(s)',default='21,22,23,80')
     args=parser.parse_args()
     host=args.host
     ports=str(args.port).split(',')
